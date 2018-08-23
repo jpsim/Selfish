275f8b66c5795947cab62c6c19d91b4b82f29904
Keith Smiley <keithbsmiley@gmail.com>
2018-08-23 10:52:17 -0700

    Add Yams

--- Package.swift
+++ Package.swift
@@ -7,9 +7,10 @@ let package = Package(
         .executable(name: "selfish", targets: ["selfish"])
     ],
     dependencies: [
-        .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.21.1")
+        .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.21.1"),
+        .package(url: "https://github.com/jpsim/Yams.git", from: "1.0.0")
     ],
     targets: [
-        .target(name: "selfish", dependencies: ["SourceKittenFramework"])
+        .target(name: "selfish", dependencies: ["SourceKittenFramework", "Yams"])
     ]
 )
